49fa80245c5f2006fae2a5aae84fb8c79f861798
Nicholas Coxen <nicjcoxen@gmail.com>
2022-04-10 20:50:03 +1200

    initial

---
+++ Marijuana-Arrests.R
@@ -0,0 +1,7 @@
+# Marijuana Arrests
+
+library(tidyverse)
+library(gridExtra)
+library(cowplot)
+
+data <- read_csv("https://mpdc.dc.gov/sites/default/files/dc/sites/mpdc/publication/attachments/Marijuana%20Arrest%20Data%20%282012-2019%29.csv")
